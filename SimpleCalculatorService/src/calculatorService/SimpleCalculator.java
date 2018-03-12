8c32594e7c7d88731b0194634d0c5d25946dd293
Allan Arnesen <allan.arnesen@gmail.com>
2018-03-12 11:26:21 +0100

    Init commit

---
+++ SimpleCalculatorService/src/calculatorService/SimpleCalculator.java
@@ -0,0 +1,20 @@
+package calculatorService;
+
+public class SimpleCalculator {
+
+	public double add(double t1, double t2){
+		return t1 + t2;
+	}
+	
+	public double sub(double t1, double t2){
+		return t1 - t2;
+	}
+	
+	public double mul(double t1, double t2){
+		return t1 * t2;
+	}
+	
+	public double div(double t1, double t2){
+		return t1 / t2;
+	}
+}
